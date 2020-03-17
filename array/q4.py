0c0fb58b3ce84d828f5f1dd2b6e2205d51d52cd6
Tek Huynh <paulhuynh412@hotmail.com>
2020-03-17 14:13:48 +1100

    q4 python

---
+++ array/q4.py
@@ -0,0 +1,22 @@
+def palperm(s):
+    chars = list(s.lower().replace(' ', ''))
+
+    m = {}
+
+    for c in chars:
+        if c in m:
+            m[c] += 1
+        else:
+            m[c] = 1
+
+    num_odd = 0
+    for v in m.values():
+        if v % 2 == 1:
+            num_odd += 1
+    if num_odd > 1:
+        return False
+    return True
+
+
+s = input()
+print(palperm(s))
